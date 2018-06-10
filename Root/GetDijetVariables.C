e26fe6c394b5cda8b893e32d59b183e68fa62e4b
Benjamin Hooberman <hooberman@gmail.com>
2018-06-10 11:22:31 +0200

    use unsigned int to avoid warnings

--- Root/GetDijetVariables.C
+++ Root/GetDijetVariables.C
@@ -58,8 +58,8 @@ void GetDijetVariables(TLorentzVector z_4vec, TLorentzVector met_4vec) {
 		DR_W80jj = jet0_4vec.DeltaR(jet1_4vec);
 		DPhi_METW80 = fabs(met_4vec.DeltaPhi(dijet_4vec));
 		DPhi_W80Z = fabs(z_4vec.DeltaPhi(dijet_4vec));
-		for (int j0=0;j0<jet_pT->size();j0++) {
-			for (int j1=j0+1;j1<jet_pT->size();j1++) {
+		for (unsigned int j0=0;j0<jet_pT->size();j0++) {
+			for (unsigned int j1=j0+1;j1<jet_pT->size();j1++) {
 				jet0_4vec.SetPtEtaPhiM(jet_pT->at(j0),jet_eta->at(j0),jet_phi->at(j0),jet_m->at(j0));
 				jet1_4vec.SetPtEtaPhiM(jet_pT->at(j1),jet_eta->at(j1),jet_phi->at(j1),jet_m->at(j1));
 				dijet_4vec = jet0_4vec + jet1_4vec;
@@ -117,7 +117,7 @@ void GetDijetVariables(TLorentzVector z_4vec, TLorentzVector met_4vec) {
 	//}
 	if (jet_pT->size()>1) {
 		double min_dPhi = 1000;
-		for (int j0=0;j0<jet_pT->size();j0++) {
+		for (unsigned int j0=0;j0<jet_pT->size();j0++) {
 			jet0_4vec.SetPtEtaPhiM(jet_pT->at(j0),jet_eta->at(j0),jet_phi->at(j0),jet_m->at(j0));
 			if (min_dPhi>abs(jet0_4vec.DeltaPhi(zmet_4vec))) {
 				min_dPhi = abs(jet0_4vec.DeltaPhi(zmet_4vec));
@@ -125,7 +125,7 @@ void GetDijetVariables(TLorentzVector z_4vec, TLorentzVector met_4vec) {
 			}
 		}
 		min_dPhi = 1000;
-		for (int j1=0;j1<jet_pT->size();j1++) {
+		for (unsigned int j1=0;j1<jet_pT->size();j1++) {
 			jet1_4vec.SetPtEtaPhiM(jet_pT->at(j1),jet_eta->at(j1),jet_phi->at(j1),jet_m->at(j1));
 			if (min_dPhi>abs(jet1_4vec.DeltaPhi(zmet_4vec))) {
 				if (j1!=Wmin_j0) {
@@ -145,7 +145,7 @@ void GetDijetVariables(TLorentzVector z_4vec, TLorentzVector met_4vec) {
 		DR_Wmin2Jet = jet0_4vec.DeltaR(jet1_4vec);
 	}
 	jet_isW01->clear();
-	for (int j0=0;j0<jet_pT->size();j0++) {
+	for (unsigned int j0=0;j0<jet_pT->size();j0++) {
 		jet_isW01->push_back(0);
 	}
 	if (jet_pT->size()>1) {
@@ -153,7 +153,7 @@ void GetDijetVariables(TLorentzVector z_4vec, TLorentzVector met_4vec) {
 		jet_isW01->at(1) = 1;
 	}
 	jet_isW12->clear();
-	for (int j0=0;j0<jet_pT->size();j0++) {
+	for (unsigned int j0=0;j0<jet_pT->size();j0++) {
 		jet_isW12->push_back(0);
 	}
 	if (jet_pT->size()>2) {
@@ -163,7 +163,7 @@ void GetDijetVariables(TLorentzVector z_4vec, TLorentzVector met_4vec) {
 	jet_isWmin->clear();
 	jet_isWminJ0->clear();
 	jet_isWminJ1->clear();
-	for (int j0=0;j0<jet_pT->size();j0++) {
+	for (unsigned int j0=0;j0<jet_pT->size();j0++) {
 		jet_isWmin->push_back(0);
 		jet_isWminJ0->push_back(0);
 		jet_isWminJ1->push_back(0);
@@ -175,7 +175,7 @@ void GetDijetVariables(TLorentzVector z_4vec, TLorentzVector met_4vec) {
 		jet_isWminJ1->at(Wmin_j1) = 1;
 	}
 	isr_4vec.SetPtEtaPhiM(0,0,0,0);
-	for (int j=0;j<jet_pT->size();j++) {
+	for (unsigned int j=0;j<jet_pT->size();j++) {
 		if (jet_isW12->at(j)==0) {
 			obj_4vec.SetPtEtaPhiM(jet_pT->at(j),jet_eta->at(j),jet_phi->at(j),jet_m->at(j));
 			isr_4vec += obj_4vec;
@@ -184,7 +184,7 @@ void GetDijetVariables(TLorentzVector z_4vec, TLorentzVector met_4vec) {
 	DPhi_METNonW12Jet = fabs(met_4vec.DeltaPhi(isr_4vec));
 	NonW12Jet_pT = isr_4vec.Pt();
 	isr_4vec.SetPtEtaPhiM(0,0,0,0);
-	for (int j=0;j<jet_pT->size();j++) {
+	for (unsigned int j=0;j<jet_pT->size();j++) {
 		if (jet_isWmin->at(j)==0) {
 			obj_4vec.SetPtEtaPhiM(jet_pT->at(j),jet_eta->at(j),jet_phi->at(j),jet_m->at(j));
 			isr_4vec += obj_4vec;
@@ -193,7 +193,7 @@ void GetDijetVariables(TLorentzVector z_4vec, TLorentzVector met_4vec) {
 	DPhi_METNonWminJet = fabs(met_4vec.DeltaPhi(isr_4vec));
 	NonWminJet_pT = isr_4vec.Pt();
 	jet_isW80->clear();
-	for (int j0=0;j0<jet_pT->size();j0++) {
+	for (unsigned int j0=0;j0<jet_pT->size();j0++) {
 		jet_isW80->push_back(0);
 	}
 	if (jet_pT->size()>1) {
@@ -201,7 +201,7 @@ void GetDijetVariables(TLorentzVector z_4vec, TLorentzVector met_4vec) {
 		jet_isW80->at(W_j1) = 1;
 	}
 	isr_4vec.SetPtEtaPhiM(0,0,0,0);
-	for (int j=0;j<jet_pT->size();j++) {
+	for (unsigned int j=0;j<jet_pT->size();j++) {
 		if (jet_isW01->at(j)==0) {
 			obj_4vec.SetPtEtaPhiM(jet_pT->at(j),jet_eta->at(j),jet_phi->at(j),jet_m->at(j));
 			isr_4vec += obj_4vec;
