f43527577c98092e8026b7da87af4a27c631dda4
Benjamin Hooberman <hooberman@gmail.com>
2018-07-11 18:25:23 +0200

    update smearing paths, change selection (remove Z_pt>37 GeV cut and increase njets to >=2)

--- Root/GetMllHistogram.C
+++ Root/GetMllHistogram.C
@@ -3,9 +3,14 @@ TH1D* hist_Mll_dPt[bin_size][dpt_bin_size];
 TH1D* hist_low_dpt = new TH1D("hist_low_dpt","",dpt_bin_size,dpt_bin);
 TH1D* hist_sm_pt = new TH1D("hist_sm_pt","",bin_size,sm_pt_bin);
 
-void GetMllHistogram(string ch) {
+void GetMllHistogram(string ch,string period) {
 
-	float Z_truthPt_dilep = 0;
+        string mcperiod = "";
+        if( TString(period).EqualTo("data15-16") ) mcperiod = "ZMC16a";
+        if( TString(period).EqualTo("data17")    ) mcperiod = "ZMC16cd";
+	if( TString(period).EqualTo("data18")    ) mcperiod = "ZMC16cd";
+		
+	//double Z_truthPt_dilep = 0;
 	//for (int bin=0;bin<dpt_bin_size;bin++) {
 	//	hist_Mll_dPt[bin] = new TH1D(TString("hist_Mll_dPt_")+TString::Itoa(bin,10),"",mll_bin_size,mll_bin);
 	//}
@@ -14,31 +19,55 @@ void GetMllHistogram(string ch) {
 			hist_Mll_dPt[bin0][bin1] = new TH1D(TString("hist_Mll_dPt_")+TString::Itoa(bin0,10)+TString("_")+TString::Itoa(bin1,10),"",mll_bin_size,mll_bin);
 		}
 	}
-	TFile fZ( TString(R20smearingPath)+"zjets/zjets_"+TString(ch)+".root" );
+
+	//--- R20.7 samples
+	//string filename = oldSmearingPath + "zjets_221/zjets_" + ch + ".root";
+
+	//--- R21 samples
+	string filename = smearingPath + mcperiod + "/" + "Zjets_merged_processed.root";
+
+	cout << "Opening mll histo file : " << filename << endl;
+	
+	TFile fZ( filename.c_str() );
+	//TFile fZ( TString(smearingPath)+"zjets_221/zjets_"+TString(ch)+".root" );
 	TTree*  tZ              = (TTree*)fZ.Get("BaselineTree");
 	tZ->SetBranchStatus("*", 0);
 	tZ->SetBranchStatus("totalWeight", 1);
+
+	tZ->SetBranchStatus("METl", 1);
 	tZ->SetBranchStatus("jet_n", 1);
 	tZ->SetBranchStatus("bjet_n", 1);
 	tZ->SetBranchStatus("Z_pt", 1);
-	tZ->SetBranchStatus("Z_truthPt", 1);
+	//tZ->SetBranchStatus("Z_truthPt", 1);
 	tZ->SetBranchStatus("HT", 1);
-	tZ->SetBranchStatus("METl", 1);
 	tZ->SetBranchStatus("mll", 1);
 	tZ->SetBranchStatus("lep_pT" ,1);
+
+	//--- double
 	tZ->SetBranchAddress("totalWeight" ,&totalWeight);
+	//tZ->SetBranchAddress("HT" ,&HT);
+	//tZ->SetBranchAddress("METl" ,&METl);
+
+	//--- double
+	//tZ->SetBranchAddress("totalWeight" ,&totalWeightF2);
+	//tZ->SetBranchAddress("HT" ,&HTF);
+	tZ->SetBranchAddress("METl" ,&METl);
+
 	tZ->SetBranchAddress("jet_n" ,&jet_n);
 	tZ->SetBranchAddress("bjet_n" ,&bjet_n);
 	tZ->SetBranchAddress("Z_pt" ,&Z_pt);
-	tZ->SetBranchAddress("Z_truthPt" ,&Z_truthPt);
-	tZ->SetBranchAddress("HT" ,&HT);
-	tZ->SetBranchAddress("METl" ,&METl);
+	//tZ->SetBranchAddress("Z_truthPt" ,&Z_truthPt);
 	tZ->SetBranchAddress("mll" ,&mll);
 	tZ->SetBranchAddress("lep_pT" ,&lep_pT           );
+	tZ->SetBranchAddress("channel" ,&channel           );
 	for (int entry=0;entry<tZ->GetEntries();entry++) {
 		tZ->GetEntry(entry);
-		if (jet_n==0) continue;
-		if (Z_pt<37.) continue;
+
+		//if( TString(ch).EqualTo("ee") && channel != 1 ) continue; // ee
+		//if( TString(ch).EqualTo("mm") && channel != 0 ) continue; // ee
+		//if (jet_n==0) continue;
+		if (jet_n<2) continue;
+		//if (Z_pt<37.) continue;
 		if (lep_pT->at(0)<leading_lep_pt_cut) continue;
 		if (lep_pT->at(1)<second_lep_pt_cut) continue;
 		//int pt = hist_low_pt->FindBin(Z_pt)-1;
