7d5ee3d0ade39fc8b4e3b907f80248660d1e7a8d
Benjamin Hooberman <hooberman@gmail.com>
2018-09-27 21:14:57 +0200

    scripts to run photon method

--- Run/run_Step1_GetPhotonEvents.sh
+++ Run/run_Step1_GetPhotonEvents.sh
@@ -2,23 +2,28 @@
 ### JETM4 data
 ###---------------------------------
 
-datapath='/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.3/JETM4/JETM4_Data/JETM4_Data_SEPT12_v1/merged/'
+datapath='/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.3/JETM4_lowpt/JETM4_Data/JETM4_Data_SEPT12_v2/merged/'
 
 for period in data15-16 data17 data18
 do
     echo "Doing JETM4 data " $period
+    treename=$period
+    #treename='tree_NoSys'
+    #treename='data'
+    #treename='tree_NoSys'
     rm -f logfiles/run_Step1_GetPhotonEvents_JETM4_Data_${period}.log
-    root -l -b -q '../Root/GetPhotonEvents.C+("'${period}'_merged_processed","gdata","'${datapath}'",1,"'${period}'")' > logfiles/run_Step1_GetPhotonEvents_JETM4_Data_${period}.log 2>&1 &
+    root -l -b -q '../Root/GetPhotonEvents.C+("'${period}'_merged_processed","gdata","'${datapath}'",1,"'${treename}'")' > logfiles/run_Step1_GetPhotonEvents_JETM4_Data_${period}.log 2>&1 &
 done
-#
-#sleep 30
-#
+
+
 ###---------------------------------
 ### JETM4 mc16a
 ###---------------------------------
 
-mc16apath='/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.3/JETM4/JETM4_mc16a/JETM4_mc16a_SEPT12_v1/merged/'
+mc16apath='/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.3/JETM4_lowpt/JETM4_mc16a/JETM4_mc16a_SEPT12_v2/merged/'
 
+
+#for sample in SinglePhoton222
 for sample in Vgamma SinglePhoton222 SinglePhoton211
 do
     echo "Doing JETM4 mc16a " $sample
@@ -31,7 +36,7 @@ done
 ### JETM4 mc16cd
 ###---------------------------------
 
-mc16cdpath='/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.3/JETM4/JETM4_mc16cd/JETM4_mc16cd_SEPT12_v1_COPY/merged/'
+mc16cdpath='/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.3/JETM4_lowpt/JETM4_mc16cd/JETM4_mc16cd_SEPT12_v1_COPY/merged/'
 
 for sample in Vgamma SinglePhoton222 SinglePhoton211
 do
@@ -39,4 +44,5 @@ do
     rm -f logfiles/run_Step1_GetPhotonEvents_JETM4_mc16cd_${sample}.log
     root -l -b -q '../Root/GetPhotonEvents.C+("'${sample}'_merged_processed","gmc16cd","'${mc16cdpath}'",0,"'${sample}'_NoSys")' > logfiles/run_Step1_GetPhotonEvents_JETM4_mc16cd_${sample}.log 2>&1 &
 done
+
 
--- Run/run_Step2_GetBaseLineEvents.sh
+++ Run/run_Step2_GetBaseLineEvents.sh
@@ -4,37 +4,51 @@
 
 datapath='/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.3/SUSY2/SUSY2_Data/'
 
-for period in data15-16 data17 data18
+for period in data15-16 data17 data18 data15-18
 do
     echo "Doing SUSY2 data " $period
     rm -f logfiles/run_Step2_GetBaseLineEvents_SUSY2_Data_${period}.log
-    root -l -b -q '../Root/GetBaseLineEvents.C+("'${period}'_merged_processed","zdata","'${datapath}'",1,"'${period}'")' > logfiles/run_Step2_GetBaseLineEvents_SUSY2_Data_${period}.log 2>&1 &
+    treename='data'
+    root -l -b -q '../Root/GetBaseLineEvents.C+("'${period}'_merged_processed","zdata","'${datapath}'",1,"'${treename}'")' > logfiles/run_Step2_GetBaseLineEvents_SUSY2_Data_${period}.log 2>&1 &
 done
 
-###  ###---------------------------------
-###  ### JETM4 mc16a
-###  ###---------------------------------
-###  
-###  mc16apath='/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.3/JETM4/JETM4_mc16a/JETM4_mc16a_SEPT12_v1/merged/'
-###  
-###  for sample in Vgamma SinglePhoton222 SinglePhoton211
-###  do
-###      echo "Doing JETM4 mc16a " $sample
-###      rm -f logfiles/run_Step1_GetBaseLineEvents_JETM4_mc16a_${sample}.log
-###      root -l -b -q '../Root/GetBaseLineEvents.C+("'${sample}'_merged_processed","zmc16a","'${mc16apath}'",0,"'${sample}'_NoSys")' > logfiles/run_Step1_GetBaseLineEvents_JETM4_mc16a_${sample}.log 2>&1 &
-###  done
-###  
-###  
-###  ###---------------------------------
-###  ### JETM4 mc16cd
-###  ###---------------------------------
-###  
-###  mc16cdpath='/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.3/JETM4/JETM4_mc16cd/JETM4_mc16cd_SEPT12_v1_COPY/merged/'
-###  
-###  for sample in Vgamma SinglePhoton222 SinglePhoton211
-###  do
-###      echo "Doing JETM4 mc16cd " $sample
-###      rm -f logfiles/run_Step1_GetBaseLineEvents_JETM4_mc16cd_${sample}.log
-###      root -l -b -q '../Root/GetBaseLineEvents.C+("'${sample}'_merged_processed","zmc16cd","'${mc16cdpath}'",0,"'${sample}'_NoSys")' > logfiles/run_Step1_GetBaseLineEvents_JETM4_mc16cd_${sample}.log 2>&1 &
-###  done
-###  
+
+###---------------------------------
+### JETM4 mc16a
+###---------------------------------
+
+mc16apath='/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.3/SUSY2/SUSY2_Bkgs_mc16a/'
+
+#for sample in diboson
+#
+
+#for sample in ttbar_410472_dilep
+for sample in Zjets lowMassDY ttbar diboson
+do
+    echo "Doing JETM4 mc16a " $sample
+    rm -f logfiles/run_Step2_GetBaseLineEvents_SUSY2_mc16a_${sample}.log
+
+    # ttbar dilep only
+    #root -l -b -q '../Root/GetBaseLineEvents.C+("'${sample}'_processed","zmc16a","'${mc16apath}'",0,"ttbar_dilep_NoSys")' > logfiles/run_Step2_GetBaseLineEvents_SUSY2_mc16a_${sample}.log 2>&1 &
+
+    # nominal
+    root -l -b -q '../Root/GetBaseLineEvents.C+("'${sample}'_merged_processed","zmc16a","'${mc16apath}'",0,"'${sample}'_NoSys")' > logfiles/run_Step2_GetBaseLineEvents_SUSY2_mc16a_${sample}.log 2>&1 &
+done
+
+
+###---------------------------------
+### JETM4 mc16cd
+###---------------------------------
+
+mc16cdpath='/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.3/SUSY2/SUSY2_Bkgs_mc16cd/'
+
+for sample in Zjets lowMassDY ttbar ttbar_dilep ttbar_nonallhad diboson
+do
+    echo "Doing JETM4 mc16cd " $sample
+    rm -f logfiles/run_Step2_GetBaseLineEvents_SUSY2_mc16cd_${sample}.log
+    root -l -b -q '../Root/GetBaseLineEvents.C+("'${sample}'_merged_processed","zmc16cd","'${mc16cdpath}'",0,"'${sample}'_NoSys")' > logfiles/run_Step2_GetBaseLineEvents_SUSY2_mc16cd_${sample}.log 2>&1 &
+done
+
+
+
+
